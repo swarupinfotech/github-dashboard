1e1704544a2d4818581c44c43e5e24690bdfe051
swarupinfotech <115489346+swarupinfotech@users.noreply.github.com>
2025-06-10 11:25:52 +0530

    official

---
+++ data/user-data.js
@@ -0,0 +1,13 @@
+export const userData = {
+  githubUser: 'swarupinfotech',
+  devUsername: "swarupinfotech1",
+  github: 'https://github.com/swarupinfotech',
+  facebook: 'https://www.facebook.com/profile.php?id=100069856215966',
+  linkedIn: 'https://www.linkedin.com/in/sarup-m-1a1372288/',
+  twitter: 'https://x.com/SwarupInfotech',
+  stackOverflow: 'https://stackoverflow.com/users/22920700/swarup-infotech',
+  leetcode: "https://leetcode.com/u/lL3NvWLd4u/",
+  resume: "#",
+  skills: ['React', 'NextJS', 'Redux', 'Express', 'NestJS', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
+  timezone: '+6'
+};
